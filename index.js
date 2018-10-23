329613c9a13a2c7a2f93bd7468f1acd2db9e6e08
Adi <adibakija35@hotmail.com>
2018-10-23 18:08:59 +0000

    Completed todoList exercise

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-// Arrays to keep track of each task's state
-const taskTitles = [];
-const taskComplete = [];
 
-// Create a new task by adding to the arrays
+// Create a new task function
 // A new task will be created as incomplete
-function newTask(title) {
-  taskTitles.push(title);
-  taskComplete.push(false);
-}
-
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
+//Note the use of the word "this" which represents the parent object.
+function newTask(title, description) {
+  const task = {
+    title      : title,
+    description: description,
+    complete   : false,
+    logState: function () {
+      console.log(this);//Logs the entire object to see it's full description.
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+    markCompleted: function () {
+      this.complete = true;
+    }
+  }
+  return task;
 }
 
 // DRIVER CODE BELOW
+//Declared 3 new tasks using OOP with newTask function above.
+const task1 = newTask("Shopping", "Shopping at grocery store.");
+const task2 = newTask("Exercising", "Running, Weight-lifting and Basketball.");
+const task3 = newTask("Leisure", "Time to chillax and take a break.");
 
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
+//Modify the properties of task1
+task1.logState();//Check the status of the task
+task1.markCompleted();
+task1.logState();//Check the status of the task
 
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
+//Modify the properties of task2
+task2.logState();
+task2.markCompleted();
+task2.logState();
+
+
